9d59320f057e1c77ece730aea1312c97e5894cd8
Siva Gollapalli <sivagollapalli88@gmail.com>
2024-01-07 20:24:53 +0530

    Added direct decedants functionality to concept

--- lib/snomed_model/concept.rb
+++ lib/snomed_model/concept.rb
@@ -1,3 +1,7 @@
 module SnomedModel
-  class Concept < ActiveRecord::Base; end
+  class Concept < ActiveRecord::Base
+    def direct_decedants
+      Description.where(conceptid: Relationship.direct_decedants(id).pluck(:sourceid))
+    end
+  end
 end
--- lib/snomed_model/relationship.rb
+++ lib/snomed_model/relationship.rb
@@ -1,3 +1,10 @@
 module SnomedModel
-  class Relationship < ActiveRecord::Base; end
+  class Relationship < ActiveRecord::Base
+    IS_A_RELATION = "116680003".freeze
+
+    scope :active, -> { where(active: "1") }
+    scope :direct_decedants, lambda { |conceptid|
+      where(typeid: IS_A_RELATION).where(destinationid: conceptid)
+    }
+  end
 end
